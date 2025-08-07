9ea9d2780b00a8417a728d19de6e7cc1bf1d1754
Ayush Rawat <ayushrawat521@gmail.com>
2025-08-07 18:57:13 +0530

    Fix: Corrected backend route require and pushing all necessary files

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const routes = require('./routes/posts');
+const routes = require('./routes/blogPosts'); 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,16 +9,18 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(express.json());
 
-
 const corsOptions = {
     origin: 'https://blog-web-app-mern-snowy.vercel.app', 
     optionsSuccessStatus: 200 
 };
 app.use(cors(corsOptions)); 
 
-
 mongoose.connect(MONGODB_URI)
   .then(() => console.log('MongoDB connected successfully!'))
   .catch(err => console.error(err));
 
 app.use('/api/posts', routes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
